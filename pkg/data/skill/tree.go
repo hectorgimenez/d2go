e71e9b37c5a6308c0a82204fc55b105621020c6d
Héctor Giménez <hector.fwbz@gmail.com>
2023-05-12 22:47:31 +0900

    refactor skill tree

--- pkg/data/skill/tree.go
+++ pkg/data/skill/tree.go
@@ -1,50 +1,46 @@
 package skill
 
-var SorceressTree = Trees{
+var SorceressTree = Tree{
 	// Cold Spells
-	Tree{
-		IceBolt:       Position{Row: 0, Column: 1},
-		FrozenArmor:   Position{Row: 0, Column: 2},
-		FrostNova:     Position{Row: 1, Column: 0},
-		IceBlast:      Position{Row: 1, Column: 1},
-		ShiverArmor:   Position{Row: 2, Column: 2},
-		GlacialSpike:  Position{Row: 3, Column: 1},
-		Blizzard:      Position{Row: 4, Column: 0},
-		ChillingArmor: Position{Row: 4, Column: 2},
-		FrozenOrb:     Position{Row: 5, Column: 0},
-		ColdMastery:   Position{Row: 5, Column: 1},
-	},
+	IceBolt:       Position{Tab: 0, Row: 0, Column: 1},
+	FrozenArmor:   Position{Tab: 0, Row: 0, Column: 2},
+	FrostNova:     Position{Tab: 0, Row: 1, Column: 0},
+	IceBlast:      Position{Tab: 0, Row: 1, Column: 1},
+	ShiverArmor:   Position{Tab: 0, Row: 2, Column: 2},
+	GlacialSpike:  Position{Tab: 0, Row: 3, Column: 1},
+	Blizzard:      Position{Tab: 0, Row: 4, Column: 0},
+	ChillingArmor: Position{Tab: 0, Row: 4, Column: 2},
+	FrozenOrb:     Position{Tab: 0, Row: 5, Column: 0},
+	ColdMastery:   Position{Tab: 0, Row: 5, Column: 1},
+
 	// Lightning Spells
-	Tree{
-		ChargedBolt:      Position{Row: 0, Column: 1},
-		StaticField:      Position{Row: 1, Column: 0},
-		Telekinesis:      Position{Row: 1, Column: 2},
-		Nova:             Position{Row: 2, Column: 0},
-		Lightning:        Position{Row: 2, Column: 1},
-		ChainLightning:   Position{Row: 3, Column: 1},
-		Teleport:         Position{Row: 3, Column: 2},
-		ThunderStorm:     Position{Row: 4, Column: 0},
-		EnergyShield:     Position{Row: 4, Column: 2},
-		LightningMastery: Position{Row: 5, Column: 1},
-	},
+	ChargedBolt:      Position{Tab: 1, Row: 0, Column: 1},
+	StaticField:      Position{Tab: 1, Row: 1, Column: 0},
+	Telekinesis:      Position{Tab: 1, Row: 1, Column: 2},
+	Nova:             Position{Tab: 1, Row: 2, Column: 0},
+	Lightning:        Position{Tab: 1, Row: 2, Column: 1},
+	ChainLightning:   Position{Tab: 1, Row: 3, Column: 1},
+	Teleport:         Position{Tab: 1, Row: 3, Column: 2},
+	ThunderStorm:     Position{Tab: 1, Row: 4, Column: 0},
+	EnergyShield:     Position{Tab: 1, Row: 4, Column: 2},
+	LightningMastery: Position{Tab: 1, Row: 5, Column: 1},
+
 	// Fire Spells
-	Tree{
-		FireBolt:    Position{Row: 0, Column: 1},
-		Warmth:      Position{Row: 0, Column: 2},
-		Inferno:     Position{Row: 1, Column: 0},
-		Blaze:       Position{Row: 2, Column: 1},
-		FireBall:    Position{Row: 2, Column: 2},
-		FireWall:    Position{Row: 3, Column: 0},
-		Enchant:     Position{Row: 3, Column: 2},
-		Meteor:      Position{Row: 4, Column: 1},
-		FireMastery: Position{Row: 5, Column: 1},
-		Hydra:       Position{Row: 5, Column: 2},
-	},
+	FireBolt:    Position{Tab: 2, Row: 0, Column: 1},
+	Warmth:      Position{Tab: 2, Row: 0, Column: 2},
+	Inferno:     Position{Tab: 2, Row: 1, Column: 0},
+	Blaze:       Position{Tab: 2, Row: 2, Column: 1},
+	FireBall:    Position{Tab: 2, Row: 2, Column: 2},
+	FireWall:    Position{Tab: 2, Row: 3, Column: 0},
+	Enchant:     Position{Tab: 2, Row: 3, Column: 2},
+	Meteor:      Position{Tab: 2, Row: 4, Column: 1},
+	FireMastery: Position{Tab: 2, Row: 5, Column: 1},
+	Hydra:       Position{Tab: 2, Row: 5, Column: 2},
 }
 
-type Trees [3]Tree
 type Tree map[Skill]Position
 type Position struct {
+	Tab    int
 	Row    int
 	Column int
 }
