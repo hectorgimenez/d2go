7c1647ae685905307a3cdb55d8ce2dfe0e002c45
Héctor Giménez <hector.fwbz@gmail.com>
2024-05-29 09:17:20 +0900

    embed object name

--- pkg/data/objects.go
+++ pkg/data/objects.go
@@ -3,8 +3,8 @@ package data
 import "github.com/hectorgimenez/d2go/pkg/data/object"
 
 type Object struct {
-	ID           UnitID
-	Name         object.Name
+	ID UnitID
+	object.Name
 	IsHovered    bool
 	Selectable   bool
 	InteractType object.InteractType
