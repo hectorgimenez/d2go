ddeb350bee0177438a27b74ebd04cc81fbea106b
Farmith <93247503+Farmith@users.noreply.github.com>
2024-08-23 20:16:21 +0200

    new questptr (#38)

--- pkg/memory/game_reader.go
+++ pkg/memory/game_reader.go
@@ -32,7 +32,7 @@ func (gd *GameReader) GetData() data.Data {
 	hover := gd.hoveredData()
 
 	// Quests
-	q1 := uintptr(gd.Process.ReadUInt(gd.moduleBaseAddressPtr+0x22F69F8, Uint64))
+	q1 := uintptr(gd.Process.ReadUInt(gd.moduleBaseAddressPtr+0x22E2978, Uint64))
 	q2 := uintptr(gd.Process.ReadUInt(q1, Uint64))
 	gameQuestsBytes := gd.Process.ReadBytesFromMemory(q2, 85)
 
