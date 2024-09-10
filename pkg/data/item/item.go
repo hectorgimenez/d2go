7b42e27aba05cb8d42a40a184364c3945d98c57c
TDL <TDLofCC@gmail.com>
2024-09-10 16:04:47 +0200

    Added extra item info (#41)

--- pkg/data/item/item.go
+++ pkg/data/item/item.go
@@ -1,17 +1,31 @@
 package item
 
 type Description struct {
-	ID              int
-	Name            string
-	Code            string
-	NormalCode      string // Normal
-	UberCode        string // Exceptional
-	UltraCode       string // Elite
-	InventoryWidth  int
-	InventoryHeight int
-	MinDefense      int
-	MaxDefense      int
-	Type            string
+	ID                int
+	Name              string
+	Code              string
+	NormalCode        string // Normal
+	UberCode          string // Exceptional
+	UltraCode         string // Elite
+	InventoryWidth    int
+	InventoryHeight   int
+	MinDefense        int
+	MaxDefense        int
+	MinDamage         int
+	MaxDamage         int
+	TwoHandMinDamage  int
+	TwoHandMaxDamage  int
+	MinMissileDamage  int
+	MaxMissileDamage  int
+	Speed             int // for weapons speed is the attack speed modifier,for armor its the movement penalty
+	StrengthBonus     int
+	DexterityBonus    int
+	RequiredStrength  int
+	RequiredDexterity int
+	Durability        int
+	RequiredLevel     int
+	MaxSockets        int
+	Type              string
 }
 
 func (d Description) Tier() Tier {
