889fa020f2f6f2787a74a48af58fa62dd65f28d9
elb <51070858+elobo91@users.noreply.github.com>
2024-12-12 11:16:29 -0500

    Lastgamename/lastgamepassword (#56)
    * Lastgamename/lastgamepassword
    
    Credits to Gogigogi1
    
    * Remove //outdated ckm
    
    * Removing offset since we have direct address

--- pkg/memory/game_reader.go
+++ pkg/memory/game_reader.go
@@ -245,13 +245,11 @@ func (gd *GameReader) IsInCharacterCreationScreen() bool {
 }
 
 func (gd *GameReader) LastGameName() string {
-	// outdated
-	return gd.ReadStringFromMemory(gd.moduleBaseAddressPtr+0x29DBD10+0x8, 0)
+	return gd.ReadStringFromMemory(gd.moduleBaseAddressPtr+0x2587FB8, 0)
 }
 
 func (gd *GameReader) LastGamePass() string {
-	// outdated
-	return gd.ReadStringFromMemory(gd.moduleBaseAddressPtr+0x29DBD10+0x60, 0)
+	return gd.ReadStringFromMemory(gd.moduleBaseAddressPtr+0x2588018, 0)
 }
 
 func (gd *GameReader) FPS() int {
